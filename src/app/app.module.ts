3e8ceecaff5fd95342ed5511bc0a7c76a04f5ddd
leberwurszt <dominik.reich@onlinehome.de>
2020-06-05 15:27:47 +0200

    k

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ShowAgeComponent } from './show-age/show-age.component';
@@ -12,7 +13,8 @@ import { ShowAgeComponent } from './show-age/show-age.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/show-age/show-age.component.ts
+++ src/app/show-age/show-age.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, NgModule } from '@angular/core';
 import { AgeService } from '../age.service';
 import { Age } from '../age';
+
+enum sortation {
+  UNSORTED,
+  SORT_NAME,
+  SORT_AGE,
+  SORT_COUNT
+};
 
 @Component({
   selector: 'app-show-age',
   templateUrl: './show-age.component.html',
   styleUrls: ['./show-age.component.css']
 })
+
 export class ShowAgeComponent implements OnInit {
 
   constructor(public ageService: AgeService) { }
   public age: Object = new Array();
   public ages: Object[] = new Array();
+  public newName: string = "";
+  public sortBy: sortation = sortation.UNSORTED;
   private names: string[] = [
     'Hans',
     'Peter',
@@ -21,37 +31,88 @@ export class ShowAgeComponent implements OnInit {
     'Mandy',
     'Sarah',
     'Michael'
-  ]
+  ];
+
+  ngOnInit(): void {
+    this.getAges(this.names);
+  }
+
+
+  clickRemoveName(age: Object): void {
+    const index: number = this.ages.indexOf(age);
+    if (index !== -1) {
+        this.ages.splice(index, 1);
+    }        
+  }
+
+  clickAddName(): void {
+    if(this.newName != "")
+      this.getAge(this.newName);
+    this.newName = "";
+  }
 
   clickName(): void {
-    this.ages.sort((a, b) => a['name'].localeCompare(b['name']));
+    this.sortBy=sortation.SORT_NAME;
+    this.sort();
   }
 
   clickAge(): void {
-    this.ages.sort((a, b) => a['age'] - b['age']);
+    this.sortBy=sortation.SORT_AGE;
+    this.sort();
   }
 
   clickCount(): void {
-    this.ages.sort((a, b) => a['count'] - b['count']);
-  }
-  
-  ngOnInit(): void {
-    
-    /*
-    this.names.forEach(name => {
-      this.getAge(name);
-      
-    });
-    */
-    
-    this.getAges(this.names);
-    //console.log(this.ages);
-    
-    //this.getAge("Hans");
-    //console.log(this.names);
-    //console.log("a");
+    this.sortBy=sortation.SORT_COUNT;
+    this.sort();
   }
 
+  // Sort items by selected sortation
+  sort(): void {
+    switch (this.sortBy) {
+      case sortation.UNSORTED:
+        break;
+      
+      case sortation.SORT_AGE:
+        this.ages.sort((a, b) => a['age'] - b['age']);
+        break;
+      
+      case sortation.SORT_COUNT:
+        this.ages.sort((a, b) => a['count'] - b['count']);
+        break;
+
+      case sortation.SORT_NAME:
+        this.ages.sort((a, b) => a['name'].localeCompare(b['name']));
+        break;
+    }
+  }
+
+  // Get class for CSS stylesheet depending on sortation
+  getThClass(index: number): string {
+    switch(index) {
+      case 0:
+        if (this.sortBy == sortation.SORT_NAME)
+          return "thSorted";
+        else
+          return "thUnsorted";
+
+      case 1:
+        if (this.sortBy == sortation.SORT_AGE)
+          return "thSorted";
+        else
+          return "thUnsorted";
+
+      case 2:
+        if (this.sortBy == sortation.SORT_COUNT)
+          return "thSorted";
+        else
+          return "thUnsorted";
+
+      default:
+        return "thUnsorted";
+    }
+  }
+
+  // Get items from default name array
   getAges(names: string[]) {
     this.ageService.getAges(names).subscribe(
       data => {
@@ -65,10 +126,14 @@ export class ShowAgeComponent implements OnInit {
       },
       //data => { console.log(this.ages.push(data))},
       err => console.error(err),
-      () => {console.log('done loading age')}
+      () => {
+        console.log('done loading age');
+        this.sort();
+    }
     );
   }
 
+  // Get new item
   getAge(name: string) {
     this.ageService.getAge(name).subscribe(
       //data => { console.log(data)},
@@ -77,7 +142,10 @@ export class ShowAgeComponent implements OnInit {
       },
       //data => { console.log(this.ages.push(data))},
       err => console.error(err),
-      () => {console.log('done loading age')}
+      () => {
+        console.log('done loading age');
+        this.sort();
+    }
     );
   }
 }
